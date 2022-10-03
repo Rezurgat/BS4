945aca212fd02932d35de3c064a5c3294861c7fe
Rezurgat <Killa.S.V.ru@bk.ru>
2022-10-03 20:49:46 +0300

    Continue work with medicine parser

--- blank/medicine/medicine.py
+++ blank/medicine/medicine.py
@@ -26,7 +26,28 @@ for item in all_codes:
     item_text = item.text
     item_href = 'https://www.icd10data.com' + item.get('href')
     all_codes_dict[item_text] = [item_href]
-print(all_codes_dict)
 
 
+# with open('all_codes.json', 'w', encoding='utf-8') as file:
+#     json.dump(all_codes_dict, file, indent=4, ensure_ascii=False)
 
+with open('all_codes.json', encoding='utf-8') as file:
+    codes_categories = json.load(file)
+
+for category_name, category_href in codes_categories.items():
+
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
+
+    # with open(f'data/{category_name}.html', 'w', encoding='utf-8') as file:
+    #     file.write(src)
+
+    with open(f'data/{category_name}.html', encoding='utf-8') as file:
+        page = file.read()
+
+    soup = BeautifulSoup(page, 'lxml')
+
+    # group_code = soup.find('div', class_='body-content').find('h1', class_='pageHeading').find('span', class_='identifier').text
+    # group_desc = soup.find('h1', class_='pageHeading').text.replace(group_code, '')
+    # code = soup.find('ul', class_='i51').find('li').find_all(class_='identifier')
+    # print(code)
